9297ee32b47cc1abe47a8421b804a8890179843d
Stephane Kiss <skiss999@gmail.com>
2024-10-23 14:25:31 -0700

    Minor edit

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -3,15 +3,13 @@ vim.o.tabstop = 2
 vim.o.shiftwidth = 2
 vim.o.expandtab = true
 vim.o.list = true
-vim.o.ignorecase = true
-vim.opt.fillchars = { eob = " " }
-
-vim.o.confirm = true
-vim.o.swapfile = false
-
 vim.o.number = true
 vim.o.relativenumber = true
 
+vim.o.ignorecase = true
+vim.opt.fillchars = { eob = " " }
+vim.o.confirm = true
+vim.o.swapfile = false
 vim.o.clipboard = vim.env.SSH_TTY and "" or "unnamedplus" -- Sync with system clipboard
 
 vim.g.nord_italic = false
