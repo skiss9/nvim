b20c444ae2cf7ba6758712e4a232f02aa3e6f9a3
Stephane Kiss <skiss999@gmail.com>
2024-10-23 01:09:38 -0700

    Focus on current buffer in nvim-tree

--- lua/plugins/tree.lua
+++ lua/plugins/tree.lua
@@ -2,6 +2,9 @@ return {
   {
     "nvim-tree/nvim-tree.lua",
     opts = {
+      update_focused_file = {
+        enable = true,
+      },
       sort = {
         sorter = "case_sensitive",
       },
