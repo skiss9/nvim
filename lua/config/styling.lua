9ec6fab21a913955f195e1e91c893d8600b4150c
Stephane Kiss <skiss999@gmail.com>
2024-10-24 19:32:35 -0700

    Edit colors

--- lua/config/styling.lua
+++ lua/config/styling.lua
@@ -1,6 +1,8 @@
 vim.api.nvim_set_hl(0, "NvimTreeNormal", { bg = "#1a1b26" })
 vim.api.nvim_set_hl(0, "NvimTreeHeader", { bg = "#1a1b26" })
 
-vim.api.nvim_set_hl(0, "Search", { fg = "#FFFFFF", bg = "#3D757D" })
+vim.api.nvim_set_hl(0, "Search", { fg = "#FFFFFF", bg = "#19AFB7" })
 vim.api.nvim_set_hl(0, "CurSearch", { fg = "#FFFFFF", bg = "#E65E9E" })
 vim.api.nvim_set_hl(0, "IncSearch", { fg = "#FFFFFF", bg = "#1E8A90" })
+
+vim.api.nvim_set_hl(0, "FlashLabel", { fg = "#000000", bg = "#ECEFF4" })
