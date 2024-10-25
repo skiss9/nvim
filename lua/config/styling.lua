eddbe2edd3973863bf54387d134b2b1f0b4b167f
Stephane Kiss <skiss999@gmail.com>
2024-10-24 18:55:07 -0700

    Change highlight colors

--- lua/config/styling.lua
+++ lua/config/styling.lua
@@ -1,2 +1,6 @@
 vim.api.nvim_set_hl(0, "NvimTreeNormal", { bg = "#1a1b26" })
 vim.api.nvim_set_hl(0, "NvimTreeHeader", { bg = "#1a1b26" })
+
+vim.api.nvim_set_hl(0, "Search", { fg = "#FFFFFF", bg = "#3D757D" })
+vim.api.nvim_set_hl(0, "CurSearch", { fg = "#FFFFFF", bg = "#E65E9E" })
+vim.api.nvim_set_hl(0, "IncSearch", { fg = "#FFFFFF", bg = "#1E8A90" })
