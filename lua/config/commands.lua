b2393fffa8c898076b97d8f9de6c0f1789baaba4
Stephane Kiss <skiss999@gmail.com>
2024-10-30 01:55:05 -0700

    Fix neovide scrolling on buffer change

--- lua/config/commands.lua
+++ lua/config/commands.lua
@@ -3,3 +3,17 @@ vim.api.nvim_create_autocmd("TextYankPost", {
     vim.highlight.on_yank({ timeout = 1000 })
   end,
 })
+
+-- Fix neovide scrolling on buffer change
+vim.api.nvim_create_autocmd("BufLeave", {
+  callback = function()
+    vim.g.neovide_scroll_animation_length = 0
+  end,
+})
+vim.api.nvim_create_autocmd("BufEnter", {
+  callback = function()
+    vim.fn.timer_start(70, function()
+      vim.g.neovide_scroll_animation_length = 0.3
+    end)
+  end,
+})
