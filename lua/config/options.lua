31a266d6fb5552f52b9e453afcf940ce1ccc3f2b
Stephane Kiss <skiss999@gmail.com>
2024-11-03 13:44:09 -0800

    Adjust line height for neovim, minor edits

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,17 +1,18 @@
 vim.o.tabstop = 2
---vim.o.softtabstop = 2
 vim.o.shiftwidth = 2
 vim.o.expandtab = true
 vim.o.list = true
 vim.o.number = true
 vim.o.relativenumber = true
+vim.o.linespace = -4
 
 vim.o.ignorecase = true
-vim.opt.fillchars = { eob = " " }
 vim.o.confirm = true
 vim.o.swapfile = false
-vim.opt.showmode = false
 vim.o.clipboard = vim.env.SSH_TTY and "" or "unnamedplus" -- Sync with system clipboard
+
+vim.opt.showmode = false
+vim.opt.fillchars = { eob = " " }
 vim.opt.fillchars:append({ vert = " " })
 
 vim.g.neovide_cursor_animation_length = 0
