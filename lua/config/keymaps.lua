c9c69da6e18d1c71301b3287ca32b3c886b3a5c4
Stephane Kiss <skiss999@gmail.com>
2024-11-23 10:34:20 -0800

    Reorganize keymaps

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,12 +1,8 @@
 local keyfns = require("config.keyfns")
 local cmd = require("config.cmd-key")
 
-vim.keymap.set("n", "<C-j>", "<cmd>m .+1<cr>==", { desc = "Move Down" })
-vim.keymap.set("n", "<C-k>", "<cmd>m .-2<cr>==", { desc = "Move Up" })
-vim.keymap.set("i", "<C-j>", "<esc><cmd>m .+1<cr>==gi", { desc = "Move Down" })
-vim.keymap.set("i", "<C-k>", "<esc><cmd>m .-2<cr>==gi", { desc = "Move Up" })
-vim.keymap.set("v", "<C-j>", ":m '>+1<cr>gv=gv", { desc = "Move Down" })
-vim.keymap.set("v", "<C-k>", ":m '<-2<cr>gv=gv", { desc = "Move Up" })
+vim.keymap.set({ "n", "v" }, cmd("j"), "15<C-e>M", { desc = "Scroll down" })
+vim.keymap.set({ "n", "v" }, cmd("k"), "15<C-y>M", { desc = "Scroll up" })
 
 vim.keymap.set("n", "q", "V", { desc = "Switch to visual line mode" })
 vim.keymap.set("v", "q", "<esc>", { desc = "Exit visual line mode" })
@@ -15,8 +11,20 @@ vim.keymap.set({ "n", "v" }, "<leader>q", "q", { desc = "Macro" })
 vim.keymap.set({ "n", "v" }, ",", "{", { desc = "Move to previous paragraph" })
 vim.keymap.set({ "n", "v" }, "m", "}", { desc = "Move to next paragraph" })
 
-vim.keymap.set({ "n", "v" }, cmd("j"), "15<C-e>M", { desc = "Scroll down" })
-vim.keymap.set({ "n", "v" }, cmd("k"), "15<C-y>M", { desc = "Scroll up" })
+vim.keymap.set("n", cmd("p"), keyfns.select_paragraph, { desc = "Select paragraph", silent = true })
+vim.keymap.set("v", cmd("p"), "ap", { desc = "Select next paragraph" })
+vim.keymap.set("n", cmd("e"), keyfns.select_function, { desc = "Select function" })
+vim.keymap.set("v", cmd("e"), keyfns.goto_function_end, { desc = "Select next function" })
+
+vim.keymap.set({ "n", "i", "v" }, cmd("i"), keyfns.insert_line_above, { silent = true, desc = "Insert line above" })
+vim.keymap.set({ "n", "i", "v" }, cmd("u"), keyfns.insert_line_below, { silent = true, desc = "Insert line below" })
+
+vim.keymap.set("n", "<C-j>", "<cmd>m .+1<cr>==", { desc = "Move Down" })
+vim.keymap.set("n", "<C-k>", "<cmd>m .-2<cr>==", { desc = "Move Up" })
+vim.keymap.set("i", "<C-j>", "<esc><cmd>m .+1<cr>==gi", { desc = "Move Down" })
+vim.keymap.set("i", "<C-k>", "<esc><cmd>m .-2<cr>==gi", { desc = "Move Up" })
+vim.keymap.set("v", "<C-j>", ":m '>+1<cr>gv=gv", { desc = "Move Down" })
+vim.keymap.set("v", "<C-k>", ":m '<-2<cr>gv=gv", { desc = "Move Up" })
 
 vim.keymap.set({ "n", "v" }, cmd("Left"), "0", { desc = "Move to the beginning of the line" })
 vim.keymap.set({ "n", "v" }, cmd("Right"), "$", { desc = "Move to the end of the line" })
@@ -43,17 +51,9 @@ vim.keymap.set("v", cmd("["), "<gv", { desc = "Indent left" })
 vim.keymap.set("n", cmd("]"), "V>gv<esc>", { desc = "Indent right", silent = true })
 vim.keymap.set("n", cmd("["), "V<gv<esc>", { desc = "Indent left", silent = true })
 
-vim.keymap.set("n", cmd("p"), keyfns.select_paragraph, { desc = "Select paragraph", silent = true })
-vim.keymap.set("v", cmd("p"), "ap", { desc = "Select next paragraph" })
-vim.keymap.set("n", cmd("e"), keyfns.select_function, { desc = "Select function" })
-vim.keymap.set("v", cmd("e"), keyfns.goto_function_end, { desc = "Select next function" })
-
 local builtin = require("telescope.builtin")
 vim.keymap.set({ "n", "i", "v" }, cmd("f"), builtin.find_files, { desc = "Telescope find files" })
 vim.keymap.set({ "n", "i", "v" }, cmd("g"), builtin.live_grep, { desc = "Telescope live grep" })
-
-vim.keymap.set({ "n", "i", "v" }, cmd("i"), keyfns.insert_line_above, { silent = true, desc = "insert line above" })
-vim.keymap.set({ "n", "i", "v" }, cmd("u"), keyfns.insert_line_below, { silent = true, desc = "Insert line below" })
 
 vim.keymap.set({ "n", "v" }, "<leader>h", "<cmd>Telescope highlights<CR>", { desc = "Show all highlights" })
 vim.keymap.set({ "n", "v" }, "<leader>d", "<cmd>NvimWebDeviconsHiTest<CR>", { desc = "Show all Web Dev Icons" })
