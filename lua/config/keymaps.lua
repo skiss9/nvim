ae255936f8daa839a54c2a78f4d5da39d5980ce7
Stephane Kiss <skiss999@gmail.com>
2025-01-30 11:25:54 -0800

    Rearrange keymaps

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -29,23 +29,6 @@ vim.keymap.set("i", "<C-k>", "<esc><cmd>m .-2<cr>==gi", { desc = "Move up" })
 vim.keymap.set("v", "<C-j>", ":m '>+1<cr>gv=gv", { desc = "Move down" })
 vim.keymap.set("v", "<C-k>", ":m '<-2<cr>gv=gv", { desc = "Move up" })
 
-vim.keymap.set({ "n", "v" }, cmd("Left"), "0", { desc = "Move to the beginning of the line" })
-vim.keymap.set({ "n", "v" }, cmd("Right"), "$", { desc = "Move to the end of the line" })
-vim.keymap.set({ "n", "v" }, cmd("Up"), "gg0", { desc = "Move to the first line of the file" })
-vim.keymap.set({ "n", "v" }, cmd("Down"), "G0", { desc = "Move to the last line of the file" })
-
-vim.keymap.set({ "n" }, cmd("r"), keyfns.toggle_tree_buffer, { desc = "Toggle between tree and buffers" })
-vim.keymap.set({ "i", "v" }, cmd("r"), "<esc>" .. cmd("r"), { desc = "Toggle between tree and buffers", remap = true })
-
-vim.keymap.set({ "n", "i", "v" }, cmd("a"), "gg0VG", { desc = "Select all" })
-vim.keymap.set({ "n", "i", "v" }, cmd("s"), "<cmd>w<CR>", { desc = "Save", silent = true })
-vim.keymap.set({ "n", "i", "v" }, cmd("d"), keyfns.close_buffer, { desc = "Close buffer" })
-
-vim.keymap.set({ "v" }, cmd("c"), "y", { desc = "Copy" })
-vim.keymap.set({ "n", "v" }, cmd("v"), "p", { desc = "Paste" })
-vim.keymap.set({ "i" }, cmd("v"), "<esc>pa", { desc = "Paste" })
-vim.keymap.set({ "n", "i", "v" }, cmd("z"), "<C-r>", { desc = "Redo" })
-
 vim.keymap.set("n", cmd("/"), "gcc", { desc = "Comment line", remap = true })
 vim.keymap.set("i", cmd("/"), "<esc>gcc", { desc = "Comment line", remap = true })
 
@@ -55,13 +38,23 @@ vim.keymap.set("v", cmd("["), "<gv", { desc = "Indent left" })
 vim.keymap.set("n", cmd("]"), "V>gv<esc>", { desc = "Indent right", silent = true })
 vim.keymap.set("n", cmd("["), "V<gv<esc>", { desc = "Indent left", silent = true })
 
-vim.keymap.set({ "n", "v" }, "<leader>d", vim.lsp.buf.definition, { desc = "LSP go to definition" })
-vim.keymap.set({ "n", "v" }, "<leader>r", vim.lsp.buf.rename, { desc = "LSP rename" })
+vim.keymap.set({ "v" }, cmd("c"), "y", { desc = "Copy" })
+vim.keymap.set({ "n", "v" }, cmd("v"), "p", { desc = "Paste" })
+vim.keymap.set({ "i" }, cmd("v"), "<esc>pa", { desc = "Paste" })
+vim.keymap.set({ "n", "i", "v" }, cmd("z"), "<C-r>", { desc = "Redo" })
+
+vim.keymap.set({ "n", "i", "v" }, cmd("a"), "gg0VG", { desc = "Select all" })
+vim.keymap.set({ "n", "i", "v" }, cmd("s"), "<cmd>w<CR>", { desc = "Save", silent = true })
+vim.keymap.set({ "n", "i", "v" }, cmd("d"), keyfns.close_buffer, { desc = "Close buffer" })
+vim.keymap.set({ "n" }, cmd("r"), keyfns.toggle_tree_buffer, { desc = "Toggle between tree and buffers" })
+vim.keymap.set({ "i", "v" }, cmd("r"), "<esc>" .. cmd("r"), { desc = "Toggle between tree and buffers", remap = true })
 
 local builtin = require("telescope.builtin")
 vim.keymap.set({ "n", "i", "v" }, cmd("f"), builtin.find_files, { desc = "Telescope find files" })
 vim.keymap.set({ "n", "i", "v" }, cmd("g"), builtin.live_grep, { desc = "Telescope live grep" })
 
+vim.keymap.set({ "n", "v" }, "<leader>d", vim.lsp.buf.definition, { desc = "LSP go to definition" })
+vim.keymap.set({ "n", "v" }, "<leader>r", vim.lsp.buf.rename, { desc = "LSP rename" })
 vim.keymap.set({ "n", "v" }, "<leader>h", "<cmd>Telescope highlights<CR>", { desc = "Show all highlights" })
 vim.keymap.set({ "n", "v" }, "<leader>w", "<cmd>NvimWebDeviconsHiTest<CR>", { desc = "Show all Web Dev Icons" })
 vim.keymap.set({ "n", "v" }, "<leader>i", "<cmd>Inspect<CR>", { desc = "Inspect element with treesitter" })
