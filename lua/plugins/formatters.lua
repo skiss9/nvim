f2f3c249a1a673d57abbff89623c12a0755a6df4
Stephane Kiss <skiss999@gmail.com>
2025-02-09 18:43:24 -0800

    Add formatting for typescript

--- lua/plugins/formatters.lua
+++ lua/plugins/formatters.lua
@@ -28,6 +28,7 @@ return {
       formatters_by_ft = {
         lua = { "stylua" },
         javascript = { "prettier", stop_after_first = true },
+        typescript = { "prettier", stop_after_first = true },
       },
       format_on_save = { timeout_ms = 500 },
     },
