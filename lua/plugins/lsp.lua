5bb3846cd9b80d0729a0d9152f57f2b287f90b43
Stephane Kiss <skiss999@gmail.com>
2026-01-10 09:54:25 -0800

    Fix issue where LSP was not taking into account file changes in svelte components

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -16,7 +16,19 @@ return {
       })
       lspconfig.eslint.setup({})
       lspconfig.ts_ls.setup({})
-      lspconfig.svelte.setup({})
+      lspconfig.svelte.setup({
+        on_attach = function(client)
+          -- Create an autocmd that triggers when you save a JS or TS file
+          vim.api.nvim_create_autocmd("BufWritePost", {
+            pattern = { "*.js", "*.ts" },
+            group = vim.api.nvim_create_augroup("SvelteDidChangeTsOrJsFile", { clear = true }),
+            callback = function(ctx)
+              -- Notify the Svelte language server to recheck types
+              client.notify("$/onDidChangeTsOrJsFile", { uri = ctx.match })
+            end,
+          })
+        end,
+      })
       lspconfig.lua_ls.setup({})
     end,
   },
